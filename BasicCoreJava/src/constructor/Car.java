e2c6a698724ea04c78b905bb29eab96e77ec43d4
Amey Tambe <amey_iaf@yahoo.co.in>
2020-01-31 10:05:46 +0530

    constructor

--- BasicCoreJava/src/constructor/Car.java
+++ BasicCoreJava/src/constructor/Car.java
@@ -10,5 +10,13 @@ public class Car {
 	public void features() {
 		System.out.println("Congratualtions on your new "+wheels+" wheeler, a "+colour+" car named "+model);
 	}
+	
+	
+	public Car() {
+		
+		//every class has a constructor, its default constructor is with the same name
+		//Costructor are used to initialise an object of a class
+		//Constructor doesnt have a return type
+	}
 
 }
